00a2dc756e4639fd794f314410fa0f10462a0792
antimatter15 <antimatter15@gmail.com>
2010-12-01 22:48:33 -0500

    sir dr stephen tiberius colbert dfa

--- dragupload.js
+++ dragupload.js
@@ -1,8 +1,10 @@
 function initialize(doc){
+if(!doc){ console.log('no document'); return}
+
 var isDragging = false;
 var dropTargets = [];
 var lastDrag = 0;
-
+var postMessageHeader = '!/__drag2up-$/!'; //crammed a bunch of random characters
 
 function clearTargets(){
   isDragging = false;
@@ -16,12 +18,15 @@ function clearTargets(){
 
 // determines if an element can be droppable based on several conditions
 function isDroppable(el){
-  var tag = el.tagName.toLowerCase(), pos = findPos(el);
-  //ensure that that element is on top
-  if(el.ownerDocument.elementFromPoint(pos[0]+1, pos[1]+1) != el) return false;
+  var tag = el.tagName.toLowerCase(), A;
   //if(tag == 'div' && dropTargets.indexOf(el) != -1) return 3;
-  if(((tag == 'input' && el.type.toLowerCase() == 'text') || tag == 'textarea') && (el.disabled == false && el.readOnly == false)) return 1;
-  if(el.isContentEditable && (el.parentNode.isContentEditable == false || tag == 'body')) return 2;
+  if(((tag == 'input' && el.type.toLowerCase() == 'text') || tag == 'textarea') && (el.disabled == false && el.readOnly == false)) A =  1;
+  if(el.isContentEditable && (el.parentNode.isContentEditable == false || tag == 'body')) A = 2;
+  //structured this weird way because profiling shows this part is the slowest
+  if(A){
+    var pos = findPos(el)
+    if(el.ownerDocument.elementFromPoint(pos[0]+1, pos[1]+1) == el) return A; //ensure that that element is on top
+  }
   return false;
 }
 
@@ -34,6 +39,35 @@ function findPos(obj) {
   } while (obj = obj.offsetParent);
   return [curleft,curtop];
 }
+
+function propagateMessage(msg){
+  //based on Stephen Colbert's economic "trickle down" theory. Lime flavored event messages are given to 
+  //the top 3% root windows, which flow through the system and eventually trickle down to the other 97%
+  //http://www.colbertnation.com/the-colbert-report-videos/341481/july-28-2010/the-word---ownership-society
+  window.top && top.postMessage && window.top.postMessage(postMessageHeader + 'trickle_' + msg, '*')
+  console.log(msg);
+}
+
+window.addEventListener('message', function(e){
+  if(e.data.substr(0, postMessageHeader.length) != postMessageHeader) return;
+  var data = e.data.substr(postMessageHeader.length);
+  if(data.substr(0,7) == 'trickle'){
+    //propagate downwards
+    for(var i = 0; i < frames.length; i++) frames[i] && frames[i].postMessage && frames[i].postMessage(e.data, '*');
+    var cmd = data.substr(0, 18), date = parseInt(data.substr(18), 10);
+    if(cmd == 'trickle_reactivate'){ //it should just be activate, but re seems like a good prefix to make them the same length
+      if(isDragging == false) getTargets();
+      lastDrag = date; 
+    }else if(cmd == 'trickle_deactivate'){
+      var lastBodyLeave = date;
+      setTimeout(function(){
+        if(lastDrag < lastBodyLeave){
+          clearTargets();
+        }
+      },150)
+    }
+  }
+})
 
 
 function renderTarget(el){
@@ -99,15 +133,12 @@ function renderTarget(el){
 
 
 function getTargets(){
-  var all = doc.getElementsByTagName('*');
-  for(var l = all.length; l--;){
-    if(isDroppable(all[l])){
-      renderTarget(all[l]);
-      console.log(window);
-      var x = window, z = 0;
-      while(x != top){ x = x.parent; z++}
-      console.log(z);
-      //dropTargets.push(all[l]);
+  if(doc && doc.getElementsByTagName){
+    var all = doc.getElementsByTagName('*');
+    for(var l = all.length; l--;){
+      if(isDroppable(all[l])){
+        renderTarget(all[l]);
+      }
     }
   }
 }
@@ -117,30 +148,36 @@ function getTargets(){
 
 doc.documentElement.addEventListener('dragenter', function(e){
   lastDrag = +new Date; 
-  
   if(isDragging == false && e.dataTransfer.types.indexOf('Files') != -1 && e.dataTransfer.types.indexOf('text/uri-list') == -1){
     isDragging = true;
-    setTimeout(getTargets, 500);
+    propagateMessage('reactivate'+lastDrag);
+    setTimeout(getTargets, 50);
   }
 
 }, false);
 
 doc.documentElement.addEventListener('dragover', function(e){
   //allow default to happen for normal drag/drops
-  console.log('dragover');
+  isDragging && propagateMessage('reactivate'+lastDrag);
   lastDrag = +new Date; 
 }, false);
 
 doc.documentElement.addEventListener('dragleave', function(e){
   var lastBodyLeave = +new Date;
-  console.log('dragleave');
   setTimeout(function(){
     if(lastDrag < lastBodyLeave){
       clearTargets();
+      propagateMessage('deactivate'+lastBodyLeave);
     }
   },50)
 }, false);
 
+
+doc.documentElement.addEventListener('mouseup', function(e){
+  var lastBodyLeave = +new Date;
+  propagateMessage('deactivate'+lastBodyLeave);
+  clearTargets();
+}, false);
 
 }
 
