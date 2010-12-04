e16921512f07c3ed0c1916d0104eb0736a68da05
antimatter15 <antimatter15@gmail.com>
2010-12-03 22:08:45 -0500

    works woot

--- hosts/pastebin.js
+++ hosts/pastebin.js
@@ -1,8 +1,12 @@
+/*
+ * uploadPastebin({data:btoa('hello world')},function(url){console.log(url)})
+ * http://pastebin.com/x43mgnhf
+ * */
 function uploadPastebin(file, callback){
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", "http://pastebin.com/api_public.php");  
 	xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-	xhr.send("paste_code="+encodeURIComponent(atob(file.data.replace(/^data.+base64,/i,'')));
+	xhr.send("paste_code="+encodeURIComponent(atob(file.data.replace(/^data.+base64,/i,''))));
 	xhr.onload = function(){
 		callback(xhr.responseText.replace(/^error:/ig,'error:'))
 	}
