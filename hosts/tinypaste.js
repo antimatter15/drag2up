1a9cf85d4812d3f36e0905ff895c7fafabdcfaab
antimatter15 <antimatter15@gmail.com>
2010-12-11 23:26:42 -0500

    tinypaste

---
+++ hosts/tinypaste.js
@@ -0,0 +1,14 @@
+//no https
+function uploadPastebin(req, callback){
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", "http://tinypaste.com/api/create.json");  
+	xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+	getText(req, function(file){
+  	xhr.send("paste="+encodeURIComponent( file.data  ));
+	})
+	xhr.onload = function(){
+
+		//callback(xhr.responseText.replace(/^error:/ig,'error:'))
+	}
+	
+}
