6b4ecbe22d2b45b08ead5f471315cfb1cf2ca5e2
antimatter15 <antimatter15@gmail.com>
2010-12-12 21:45:32 -0500

    more or less flickr works"

--- hosts/flickr.js
+++ hosts/flickr.js
@@ -1,18 +1,91 @@
 //19d4df95a040e50112bd8e49a6096b59
 //edc13066151f70ed (super secret secret)
 //http://flickr.com/services/auth/?api_key=19d4df95a040e50112bd8e49a6096b59&perms=write&api_sig=[api_sig]
-
-function uploadFlickr(req, callback){
+//https://secure.flickr.com/services
+//http://api.flickr.com/services
+function uploadFlickr(req, uploaded_fn){
+  var base = "http://flickr.com/services" //https://secure.flickr.com/services
   function auth(params){
     params.api_key = "19d4df95a040e50112bd8e49a6096b59";
     var secret = 'edc13066151f70ed';
-    params.api_sig = hex_md5(secret+Object.keys(params).sort().map(function(x){return x+params[x]})).join('')
+    params.api_sig = hex_md5(secret+Object.keys(params).sort().map(function(x){return x+params[x]}).join(''))
     return params;
   }
-  var xhr = new XMLHttpRequest();
-  xhr.open('get',"http://flickr.com/services/upload/");
-  var p = auth({auth_token: "blahblah"});
-  p.photo = req;
-  xhr.sendMultipart(p)
+
+  function params(obj){
+    var str = [];
+    for(var i in obj) str.push(i+'='+encodeURIComponent(obj[i]));
+    return str.join('&');
+  }
+  function getAuthToken(callback){
+    var authurl = base+"/auth/?"+params(auth({perms: "write"}));
+    
+    chrome.tabs.create({url: authurl}, function(tab){
+      var poll = function(){
+        chrome.tabs.get(tab.id, function(info){
+          if(/frob=(.+)/.test(info.url)){
+            var frob = info.url.match(/frob=(.+)/)[1]
+            chrome.tabs.remove(tab.id);
+            
+            var xt = new XMLHttpRequest();
+            xt.open("get", base+'/rest?'+params(auth({
+              method: 'flickr.auth.getToken',
+              frob: frob,
+              nojsoncallback: 1,
+              format: 'json'
+            })))
+            xt.onload = function(){
+              var json = JSON.parse(xt.responseText);
+              var token = json.auth.token._content;
+              localStorage.flickr_token = token;
+              console.log('magic token', token);
+              callback();
+            }
+            xt.send()
+            //localStorage.flickr_frob = frob;
+            console.log('magic frob', frob);
+          }else{
+            setTimeout(poll, 300)
+          }
+        })
+      };
+      poll();
+    })
   
+  }
+
+  
+  function uploadPhoto(){
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST',base+"/upload/");
+    var p = auth({
+      auth_token: localStorage.flickr_token, 
+      tags: "drag2up",
+      is_public: 1
+    });
+    p.photo = req;
+    xhr.onload = function(){
+      console.log(xhr.responseXML)
+      var resX = xhr.responseXML
+      if(resX.firstChild.getAttribute('stat') == 'fail'){
+        var err = resX.getElementsByTagName('err')[0];
+        if(err.getAttribute('code') == 98){
+          //invalid auth token: login now.
+          getAuthToken(uploadPhoto)
+        }else{  
+          callback("error: could not upload to flickr in uploadPhoto "+err.getAttribute(msg))
+        }
+      }else{
+        var photoid = resX.getElementsByTagName('photoid')[0];
+        console.log(photoid)
+        var pid = photoid.firstChild.nodeValue;
+        window.PID = photoid;
+      }
+    }
+    xhr.sendMultipart(p)
+    5255919773
+  }[url=http://www.flickr.com/photos/56903976@N07/5255919773/][img]http://farm6.static.flickr.com/5290/5255919773_cb1f78118d_s.jpg[/img][/url]
+[url=http://www.flickr.com/photos/56903976@N07/5255919773/]tinypaste[/url] by [url=http://www.flickr.com/people/56903976@N07/]antimatter15[/url], on Flickr
+  
+  uploadPhoto();
 }
