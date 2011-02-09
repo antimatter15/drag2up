3bab56c61286f1d8cf3d214f0da6944a0a4b1fc7
antimatter15 <antimatter15@gmail.com>
2011-02-09 01:03:53 -0500

    immio imgur etc blah blah direct link

--- data/hosts/imgur.js
+++ data/hosts/imgur.js
@@ -25,7 +25,12 @@ Hosts.imgur = function uploadImgur(req, callback){
       image: btoa(file.data)
     }, function(data){
       if(data){
-        callback(data.upload.links.original)
+        console.log(data); //i guess you could extract the deletion url manually
+        callback({
+          direct: data.upload.links.original,
+          url: data.upload.links.imgur_page,
+          thumb: data.upload.links.small_square //or maybe large_thumbnail
+        })
       }else{
         callback('error: could not upload to imgur')
       }
