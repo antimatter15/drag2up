4b1d6a6dcfbd8af1de38c63b4f2ebc29ac2d83b9
antimatter15 <antimatter15@gmail.com>
2011-02-10 00:30:32 -0500

    new dropbox file naming scheme

--- data/hosts/dropbox/dropbox.js
+++ data/hosts/dropbox/dropbox.js
@@ -7,20 +7,32 @@ Hosts.dropbox = function uploadDropbox(req, callback){
   var poll = function(){
     if(dropbox.isAccessGranted()){
       getRaw(req, function(file){
-        var fname = Math.random().toString(36).substr(2,4) + '/' + file.name;
+        var fname =  file.name;
         var folder = 'drag2up/'
+        
+        dropbox.getAccountInfo(function(user){
+        dropbox.getDirectoryMetadata('Public/'+folder + encodeURIComponent(file.name), function(json){
+          if(json.error && json.error.indexOf('not found') != -1){
+            //yay plop it on the top
+          }else{
+            fname = Math.random().toString(36).substr(2,4) + '_' + fname;
+          }
+          
+          
         dropbox.putFileContents('Public/'+folder + fname, file,
           function(){
             console.log('done uploading');
             //yay done. hopefully
-            dropbox.getAccountInfo(function(user){
               console.log('got stuffs now');
               //http://dl.dropbox.com/u/1024307/drag2up/testing.txt
               //console.log(arguments)
               var url = https()+"dl.dropbox.com/u/"+user.uid+"/"+folder+fname;
               callback({direct: url})
-            })
           });
+        })
+          
+          })
+          
       })
     }else{
       setTimeout(poll, 300);
